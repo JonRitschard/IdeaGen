f6304cd92109b17f3425bc050e26e80f5bb4a2a7
Jon <jonritschard@gmail.com>
2021-11-06 14:20:11 -0500

    Fixed bug in output so it would print the words instead of chars

--- ideagen.py
+++ ideagen.py
@@ -8,7 +8,7 @@ path_to_save = "C:/Users/Jon/Desktop/Shiz/_Code/IdeaGen/results"
 file1, file2, file3 = "list1.txt", "list2.txt", "list3.txt"
 
 #indicate how many results you would like
-results = 500
+results = 5
 
 list1, list2, list3 = [], [], []
 outputList = ""
@@ -16,23 +16,16 @@ outputList = ""
 while results > 0:
 
     #reads file1 and adds values to list1
-    with open(path_to_files + f"/{file1}", 'r') as f:
-        for line in f:
-            list1 += line
-    #strip all \n from list        
-    list1 = list1[0::2]  
-
+    with open(path_to_files + f"/{file1}",'r') as f:
+        list1 += f.read().splitlines() 
+        
     #reads file2 and adds values to list2
-    with open(path_to_files + f"/{file2}", 'r') as f:
-        for line in f:
-            list2 += line       
-    list2 = list2[0::2]
+    with open(path_to_files + f"/{file2}",'r') as f:
+        list2 += f.read().splitlines()
 
     #reads file3 and adds values to list3
-    with open(path_to_files + f"/{file3}", 'r') as f:
-        for line in f:
-            list3 += line       
-    list3 = list3[0::2]
+    with open(path_to_files + f"/{file3}",'r') as f:
+        list3 += f.read().splitlines()
     
     #concatenate values in lists
     randomizedResults = f"{random.choice(list1)} {random.choice(list2)} {random.choice(list3)} \n"
@@ -45,6 +38,8 @@ while results > 0:
 
     #-1 iteration
     results-=1
+
+print(outputList)
 
 #write to txt file at save path
 with open(path_to_save + f"/results.txt", 'w') as f:
